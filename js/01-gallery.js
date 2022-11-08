d0ac99463147c88343b1d359e5df3e6eb169d413
AlonaMaliarchyk <alenasymorot@gmail.com>
2022-11-08 23:46:45 +0400

    initial commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+//const basicLightbox = require('basiclightbox')
+//import * as basicLightbox from 'basiclightbox'
 
-console.log(galleryItems);
+
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+let setImg = [];
+
+for (let img of galleryItems) {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add("gallery__item", "gallery__link");
+    const imgGallery = document.createElement('img');
+    imgGallery.classList.add("gallery__image");
+    imgGallery.setAttribute('src', img.preview);
+    imgGallery.setAttribute('alt', img.description);
+    galleryItem.append(imgGallery);
+    setImg.push(galleryItem);
+}
+galleryContainer.append(...setImg);
+
+const button = document.querySelector('.btn');
+button.addEventListener('click', () => {
+    basicLightbox.create(`
+		<img width="1400" height="900" src="${galleryItems[1].original}">
+	`
+    ).show()
+})
+
